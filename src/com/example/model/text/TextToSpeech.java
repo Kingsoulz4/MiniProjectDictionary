8f145064578f600d18845858064737981813e47f
Dat Hoang <kingsoulz4a@gmail.com>
2021-10-18 08:12:07 +0700

    Complete version

--- src/com/example/model/text/TextToSpeech.java
+++ src/com/example/model/text/TextToSpeech.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package com.example.model.text;
 
 import com.sun.speech.freetts.Voice;
